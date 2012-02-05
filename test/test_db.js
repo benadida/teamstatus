646558ffa333596318c5e1dd6c0b0a1f60bdf83d
Ben Adida <ben@adida.net>
2012-02-05 13:20:19 -0800

    removed old test files

--- test/test_db.js
+++
@@ -1,7 +0,0 @@
-const db = require("../server/db");
-
-db.log_message("fake.host", "#noroom", "lloyd", "I love ircloggr");
-db.log_message("fake.host", "#noroom", "lloyd", "he is kind and gentle");
-db.log_message("fake.host", "#noroom", "lloyd", "may he live long,");
-db.log_message("fake.host", "#noroom", "lloyd", "and prosper");
-
