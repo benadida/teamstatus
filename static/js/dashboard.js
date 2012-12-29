dba950cfa434cf19e53dea03398c01ba50045450
Ben Adida <ben@adida.net>
2012-12-28 17:18:55 -0800

    added message formatting

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -7,9 +7,9 @@ var ROOM = m[2];
 
 // TEMPLATES
 
-var updateTemplateFirst = '<div class="row"><div class="two columns"><span class="label right">{{nick}}:</span></div><div class="ten columns">{{content}} <span style="font-size:0.7em;">[{{nice_at}}]</span></div></div>';
+var updateTemplateFirst = '<div class="row"><div class="two columns"><span class="label right">{{nick}}:</span></div><div class="ten columns">{{{content}}} <span style="font-size:0.7em;">[{{nice_at}}]</span></div></div>';
 
-var updateTemplateNext = '{{#items}}<div class="row"><div class="ten columns offset-by-two">{{content}} <span style="font-size:0.7em;">[{{nice_at}}]</span></div></div>{{/items}}<div class="row">&nbsp;</div>';
+var updateTemplateNext = '{{#items}}<div class="row"><div class="ten columns offset-by-two">{{{content}}} <span style="font-size:0.7em;">[{{nice_at}}]</span></div></div>{{/items}}<div class="row">&nbsp;</div>';
 
 var fiveStarDetailTemplate = '{{#issues}}<p>[<a target="_new" href="{{html_url}}">{{number}}</a>] {{title}}</p>{{/issues}}';
 
@@ -28,11 +28,86 @@ var THREE_STAR = repeatString(ONE_STAR, 3);
 var FOUR_STAR = repeatString(ONE_STAR, 4);
 var FIVE_STAR = repeatString(ONE_STAR, 5);
 
+// update formatting
+var linkRegex = /(https?:\/\/[^ ]+)/g;
+
+var markupTable = {
+  0x02: "b",
+  0x11: "tt",
+  0x1d: "i",
+  0x1f: "ul"
+};
+
+function formatMessage(msg) {
+  // entity encode
+  msg = $("<div/>").text(msg).html();
+
+  // make links clickable
+  msg = msg.replace(linkRegex, function (match) {
+    return '<a href="' + match + '">' + match + "</a>";
+  });
+
+  // color highlight messages
+  var openTags = [ ];
+  var msgbuf = "";
+  function closeTags(until) {
+    while (openTags.length) {
+      var t = openTags.pop();
+      msgbuf += "</" + t + ">";
+      if (until && until == t) break;
+    }
+  }
+  function isOpen(t) {
+    for (var i=0; i < openTags.length; i++) if (openTags[i] == t) return true;
+    return false;
+  }
+  function openTag(t) {
+    msgbuf += "<" + t + ">";
+    openTags.push(t);
+  }
+  for (var i =0; i < msg.length; i++) {
+    if (markupTable.hasOwnProperty(msg.charCodeAt(i))) {
+      var t = markupTable[msg.charCodeAt(i)];
+      if (isOpen(t)) closeTags(t);
+      else openTag(t);
+    } else if (msg.charCodeAt(i) == 0x0f) {
+      closeTags();
+    } else if (msg.charCodeAt(i) == 0x03) {
+      if (isOpen('span')) closeTags('irc');
+
+      // span coloring!
+      var color = undefined;
+      if (msg.charCodeAt(i+1) >= 0x30 &&  msg.charCodeAt(i+1) <= 0x39) {
+        if (msg.charCodeAt(i+2) >= 0x30 &&  msg.charCodeAt(i+2) <= 0x39) {
+          color = parseInt(msg.substr(i+1,2), 10);
+          i += 2;
+        } else {
+          color = parseInt(msg.substr(i+1,1), 10);
+          i += 1;
+        }
+      }
+      if (color != undefined && color >= 0 && color <= 15) {
+        msgbuf += "<span class=\"clr_" + color.toString() + "\">";
+        openTags.push("span");
+      }
+    } else if (msg.charCodeAt(i) < 32 && msg.charCodeAt(i) != 1) {
+      // uh oh, unknown control code!  add a question mark to output
+      msgbuf += "?(" + msg.charCodeAt(i) + ")";
+    } else {
+      msgbuf += msg.charAt(i);
+    }
+  }
+  closeTags();
+  msg = msgbuf;
+
+  return msg;
+}
 
 function addUpdatesFromUser(nick, updates) {
   // make dates nice
   updates.forEach(function(update) {
     update.nice_at = moment(update.at).fromNow();
+    update.content = formatMessage(update.content);
   });
 
   var renderedFirst = Mustache.render(updateTemplateFirst, updates[0]);
